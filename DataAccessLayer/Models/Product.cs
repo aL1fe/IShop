3c59883f69efde1765c4088469d73333e0ce399b
Alexei.Smirnov <alexei.smirnov@hys-enterprise.com>
2022-07-13 19:38:53 +0300

    Id was changed om string

--- DataAccessLayer/Models/Product.cs
+++ DataAccessLayer/Models/Product.cs
@@ -2,8 +2,8 @@ namespace DataAccessLayer.Models;
 
 public class Product
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
     public int Quantity { get; set; }
     public int Price { get; set; }
 }
