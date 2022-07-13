632f72e27c683b2847bb3763467b22eed43ebacd
Alexei.Smirnov <alexei.smirnov@hys-enterprise.com>
2022-07-13 19:39:42 +0300

    Configured DbContext & appsetings.json

--- DataAccessLayer/AppDbContext.cs
+++ DataAccessLayer/AppDbContext.cs
@@ -1,9 +1,24 @@
 using DataAccessLayer.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccessLayer;
 
 public class AppDbContext: DbContext
 {
-    public virtual DbSet<Product> Products { get; set; }
+    public DbSet<Product> Products { get; set; } = null!;
+    private IConfiguration Config { get; }
+    public AppDbContext()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+        Config = builder.Build();
+    }
+ 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = Config.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseSqlServer(connectionString);
+    }
 }
--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,9 +1,10 @@
+using DataAccessLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
